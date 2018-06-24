3f85819180a942a1e6ff2c67a87b454d37cde052
Stacy Olivas (KG7QIN) <olivas@eurisko.ws>
2018-06-23 22:04:20 -0700

    Forgot -C

--- post_ast_setup.sh
+++ post_ast_setup.sh
@@ -11,5 +11,5 @@ set -x
 /bin/cp /usr/work/AllStarLink-Asterisk-1.8/allstar/allstar/rc.updatenodelist /etc/asterisk/
 /bin/cp -R /usr/work/AllStarLink-Asterisk-1.8/allstar/configs/* /etc/asterisk/
 /bin/cp /usr/work/AllStarLink-Asterisk-1.8/allstar/id.gsm /etc/asterisk/
-/bin/tar -zxf /usr/work/AllStarLink-Asterisk-1.8/allstar/sounds/ASL-RptSounds.tgz /var/lib/asterisk/sounds/
+/bin/tar -zxf /usr/work/AllStarLink-Asterisk-1.8/allstar/sounds/ASL-RptSounds.tgz -C /var/lib/asterisk/sounds/
 
